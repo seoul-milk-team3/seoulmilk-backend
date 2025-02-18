92f726fac1929616bf1fdecb2737ee6b87982459
syjdjr <syjdjr@naver.com>
2025-02-19 02:24:27 +0900

    feat: 프로젝트 기본 gradle 설정

---
+++ src/test/java/com/seoulmilk/be/BeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seoulmilk.be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
