c0e9b872fc779b054a42be9dd17de458e28c2ae5
syjdjr <syjdjr@naver.com>
2025-02-25 01:07:04 +0900

    feat: #3 기본 Entity 작성

---
+++ src/main/java/com/seoulmilk/be/user/domain/User.java
@@ -0,0 +1,20 @@
+package com.seoulmilk.be.user.domain;
+
+import com.seoulmilk.be.global.domain.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class User extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+}
