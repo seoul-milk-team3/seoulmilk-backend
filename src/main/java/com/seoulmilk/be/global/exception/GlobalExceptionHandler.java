8dc4f0436289dde591afeb716ab4013923a9b484
yeonjy <yeonjypaper@gmail.com>
2025-02-25 01:06:44 +0900

    feat: GlobalExceptionHandler 구현
    ResponseEntityExceptionHandler를 상속받는 GlobalExceptionHandler를 구현했습니다.

---
+++ src/main/java/com/seoulmilk/be/global/exception/GlobalExceptionHandler.java
@@ -0,0 +1,69 @@
+package com.seoulmilk.be.global.exception;
+
+import com.seoulmilk.be.global.exception.errorcode.ErrorCode;
+import com.seoulmilk.be.global.exception.errorcode.GlobalErrorCode;
+import com.seoulmilk.be.global.exception.response.ErrorResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.NonNull;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.List;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+    @Override
+    public ResponseEntity<Object> handleMethodArgumentNotValid(
+            @NonNull MethodArgumentNotValidException e,
+            @NonNull HttpHeaders headers,
+            @NonNull HttpStatusCode status,
+            @NonNull WebRequest request) {
+        return handleExceptionInternal(e);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleAllException(Exception e, HttpServletRequest request) {
+        return handleExceptionInternal(GlobalErrorCode.INTERNAL_SERVER_ERROR);
+    }
+
+    private ResponseEntity<Object> handleExceptionInternal(ErrorCode errorCode) {
+        return ResponseEntity.status(errorCode.getHttpStatus())
+                .body(makeErrorResponse(errorCode));
+    }
+
+    private ErrorResponse makeErrorResponse(ErrorCode errorCode) {
+        return ErrorResponse.builder()
+                .isSuccess(false)
+                .code(errorCode.name())
+                .message(errorCode.getMessage())
+                .results(new ErrorResponse.ValidationErrors(null))
+                .build();
+    }
+
+    private ResponseEntity<Object> handleExceptionInternal(BindException e) {
+        return ResponseEntity.status(GlobalErrorCode.INVALID_PARAMETER.getHttpStatus())
+                .body(makeErrorResponse(e));
+    }
+
+    private ErrorResponse makeErrorResponse(BindException e) {
+        final List<ErrorResponse.ValidationError> validationErrorList = e.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(ErrorResponse.ValidationError::from)
+                .toList();
+
+        return ErrorResponse.builder()
+                .isSuccess(false)
+                .code(GlobalErrorCode.INVALID_PARAMETER.name())
+                .message(GlobalErrorCode.INVALID_PARAMETER.getMessage())
+                .results(new ErrorResponse.ValidationErrors(validationErrorList))
+                .build();
+    }
+}
