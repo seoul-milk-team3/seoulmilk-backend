d3bc8eca296832d8f6d6085886c30bda4156d7e2
syjdjr <syjdjr@naver.com>
2025-02-25 01:08:20 +0900

    feat: #3 기본 패키지 요소 작성

---
+++ src/main/java/com/seoulmilk/be/user/domain/init/UserInitializer.java
@@ -0,0 +1,13 @@
+package com.seoulmilk.be.user.domain.init;
+
+import com.seoulmilk.be.global.util.DummyDataInit;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.annotation.Order;
+
+@Slf4j
+@RequiredArgsConstructor
+@DummyDataInit
+@Order(1)
+public class UserInitializer {
+}
---
+++ src/main/java/com/seoulmilk/be/user/presentation/UserController.java
@@ -0,0 +1,15 @@
+package com.seoulmilk.be.user.presentation;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/user")
+@Tag(name = "User", description = "유저 관련 API")
+public class UserController {
+}
