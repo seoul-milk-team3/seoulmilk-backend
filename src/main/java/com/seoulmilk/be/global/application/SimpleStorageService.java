3876ffb50de1f5706394e155ec7b861299133b2b
syjdjr <syjdjr@naver.com>
2025-02-26 06:59:51 +0900

    feat: #3 S3Config 수정
    - 특정 구현체에서 인터페이스로 변경하여 추상화

---
+++ src/main/java/com/seoulmilk/be/global/application/SimpleStorageService.java
@@ -0,0 +1,14 @@
+package com.seoulmilk.be.global.application;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class SimpleStorageService {
+
+
+    // 파일 업로드
+    public
+    // 파일 삭제
+}
