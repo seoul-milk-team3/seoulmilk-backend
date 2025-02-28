9a5edf919bd261d55cc7ad57c625a66ec72881b7
yeonjy <yeonjypaper@gmail.com>
2025-03-01 07:27:47 +0900

    feat: #4 CustomUserDetails 구현
    - CustomUserDetails
    - CustomUserDetailsService

---
+++ src/main/java/com/seoulmilk/be/global/security/CustomUserDetails.java
@@ -0,0 +1,36 @@
+package com.seoulmilk.be.global.security;
+
+import com.seoulmilk.be.user.domain.User;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@Getter
+@RequiredArgsConstructor
+public class CustomUserDetails implements UserDetails {
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority(user.getRole().name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    public Long getUserId() {
+        return user.getId();
+    }
+}
