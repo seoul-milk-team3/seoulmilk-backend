9371d85f97ba8b3f3e111db139dd349cb6836490
syjdjr <syjdjr@naver.com>
2025-03-06 14:22:32 +0900

    feat: #18 JPARepository 기본 제공 메서드로 사용 변경

--- src/main/java/com/seoulmilk/be/tax/persistence/NtsTaxRepository.java
+++ src/main/java/com/seoulmilk/be/tax/persistence/NtsTaxRepository.java
@@ -8,6 +8,4 @@ import java.util.Optional;
 
 @Repository
 public interface NtsTaxRepository extends JpaRepository<NtsTax, Long> , NtsTaxRepositoryCustom {
-
-    Optional<NtsTax> findById(Long taxId);
 }
