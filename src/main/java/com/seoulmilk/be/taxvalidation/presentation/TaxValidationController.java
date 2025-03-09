5252656953f1554e69c026a47489e04638ca43c5
yeonjy <yeonjypaper@gmail.com>
2025-03-10 03:45:46 +0900

    feat: #36 다건 인증 API 구현

--- src/main/java/com/seoulmilk/be/taxvalidation/presentation/TaxValidationController.java
+++ src/main/java/com/seoulmilk/be/taxvalidation/presentation/TaxValidationController.java
@@ -2,14 +2,19 @@ package com.seoulmilk.be.taxvalidation.presentation;
 
 import com.seoulmilk.be.global.dto.SuccessResponse;
 import com.seoulmilk.be.taxvalidation.application.TaxValidationService;
+import com.seoulmilk.be.taxvalidation.dto.request.InvoiceValidationRequest;
 import com.seoulmilk.be.taxvalidation.dto.response.InvoiceVerificationResponse;
 import com.seoulmilk.be.taxvalidation.presentation.api.TaxValidationApi;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 import static com.seoulmilk.be.global.dto.SuccessCode.VALIDATE_TAX_INVOICE_SUCCESS;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("tax/validation")
@@ -17,22 +22,21 @@ public class TaxValidationController implements TaxValidationApi {
     private final TaxValidationService taxValidationService;
 
     @ResponseStatus(HttpStatus.OK)
-    @GetMapping("/{taxId}/pre-verified/{loginTypeLevel}")
+    @PostMapping("/pre-verified/{loginTypeLevel}")
     @Override
     public SuccessResponse<String> validateInvoicePreVerified(
-            @PathVariable final Long taxId, @PathVariable final String loginTypeLevel) {
-        taxValidationService.validateInvoicePreVerified(taxId, loginTypeLevel);
+            @RequestBody final List<InvoiceValidationRequest> request, @PathVariable final String loginTypeLevel) {
+        taxValidationService.validateInvoicesPreVerified(request, loginTypeLevel);
         return SuccessResponse.of(VALIDATE_TAX_INVOICE_SUCCESS);
     }
 
     @ResponseStatus(HttpStatus.OK)
-    @GetMapping("/{taxId}/post-verified/{loginTypeLevel}")
+    @PostMapping("/post-verified")
     @Override
-    public SuccessResponse<InvoiceVerificationResponse> validateInvoicePostVerified(
-            @PathVariable final Long taxId, @PathVariable final String loginTypeLevel) {
-        InvoiceVerificationResponse response = taxValidationService.validateInvoicePostVerified(taxId, loginTypeLevel);
-        taxValidationService.updateNtsTaxIsNormal(taxId, response.isNormal());
-        return SuccessResponse.of(VALIDATE_TAX_INVOICE_SUCCESS, response);
+    public SuccessResponse<List<InvoiceVerificationResponse>> validateInvoicePostVerified(
+            @RequestBody final List<InvoiceValidationRequest> request) {
+        List<InvoiceVerificationResponse> responses = taxValidationService.validateInvoicePostVerified(request);
+        return SuccessResponse.of(VALIDATE_TAX_INVOICE_SUCCESS, responses);
     }
 
 }
